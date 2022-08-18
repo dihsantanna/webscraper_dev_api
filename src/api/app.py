9a988a2e4be62e3f26bf18886d99b5c94e97b875
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-08-18 16:46:50 -0300

    feat: re-implement PUT /scrape route, but now starting scrape method in a new thread

--- src/api/app.py
+++ src/api/app.py
@@ -1,9 +1,12 @@
+import _thread
+
 from bson import json_util
 from dotenv import load_dotenv
 from flask import Flask, Response
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_restful import Api, Resource
+from scraper.scraper import Scraper
 
 load_dotenv()
 
@@ -22,4 +25,12 @@ class Notebooks(Resource):
         return Response(json_util.dumps(data), mimetype="application/json")
 
 
+class Scrape(Resource):
+    def put(self):
+        scraper = Scraper()
+        _thread.start_new_thread(scraper.scrape, ())
+        return {"message": "Scraping started!"}, 202
+
+
 api.add_resource(Notebooks, "/notebooks")
+api.add_resource(Scrape, "/scrape")
