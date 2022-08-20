6df554b6ee56e1427d22751d228653ad2a18414a
Diogo Sant'Anna <diogosantanna08@gmail.com>
2022-08-20 02:27:58 -0300

    fix: add error handling

--- webscraper_api/api/services/scrape.py
+++ webscraper_api/api/services/scrape.py
@@ -5,7 +5,10 @@ from webscraper_api.scraper.scraper import Scraper
 
 class ScrapeService:
     def get_scrape_order_by_id(self, order_id):
-        return mongo.db.scrape_order.find_one({"_id": ObjectId(order_id)})
+        try:
+            return mongo.db.scrape_order.find_one({"_id": ObjectId(order_id)})
+        except Exception:
+            return None
 
     def post_scrape_order(self):
         order_id = Scraper().scrape_order()
